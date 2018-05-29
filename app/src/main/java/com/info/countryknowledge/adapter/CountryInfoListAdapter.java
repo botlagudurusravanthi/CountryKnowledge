7ac1aafae90431b220bb5ef7e1764a348bfc97f1
Sravanthi Botlaguduru01 <Sravanthi_B01@ad.infosys.com>
2018-05-29 16:01:54 +0530

    Fragment and adapter added.Need to add the implementation.

---
+++ app/src/main/java/com/info/countryknowledge/adapter/CountryInfoListAdapter.java
@@ -0,0 +1,8 @@
+package com.info.countryknowledge.adapter;
+
+/**
+ * Created by Sravanthi_B01 on 5/29/2018.
+ */
+
+public class CountryInfoListAdapter {
+}
---
+++ app/src/main/java/com/info/countryknowledge/fragment/CountryInfoListFragment.java
@@ -0,0 +1,8 @@
+package com.info.countryknowledge.fragment;
+
+/**
+ * Created by Sravanthi_B01 on 5/29/2018.
+ */
+
+public class CountryInfoListFragment {
+}
