51bebcbd6b4079bf533ae60072e7b3330e6fedf7
Sravanthi Botlaguduru01 <Sravanthi_B01@ad.infosys.com>
2018-05-30 14:04:52 +0530

    Added layouts for the main screen and list row item.

--- app/src/main/java/com/info/countryknowledge/fragment/CountryInfoListFragment.java
+++ app/src/main/java/com/info/countryknowledge/fragment/CountryInfoListFragment.java
@@ -1,8 +1,55 @@
 package com.info.countryknowledge.fragment;
 
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import com.info.countryknowledge.R;
+
 /**
  * Created by Sravanthi_B01 on 5/29/2018.
  */
 
-public class CountryInfoListFragment {
+public class CountryInfoListFragment extends Fragment  {
+
+    @BindView(R.id.recycler_list)
+    RecyclerView recyclerList;
+
+    @BindView(R.id.swiperefresh)
+    SwipeRefreshLayout swipeRefreshLayout;
+
+    @BindView(R.id.error_message)
+    TextView errorMessage;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.list_fragment, container, false);
+        ButterKnife.bind(this, view);
+        errorMessage = view.findViewById(R.id.error_message);
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
 }
